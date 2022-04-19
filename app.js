0bd37d795533ea31ea29104061775d293d458f2d
Jonathan Cushing <firex42069@gmail.com>
2022-04-18 20:48:28 -0400

    complete html output

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 const inquirer = require("inquirer");
-/*const fs = require("fs");
+const fs = require("fs");
 const generatePage = require("./src/page-template");
-const pageHTML = generatePage(name, github);
-
-fs.writeFile("./index.html", pageHTML, err => {
-    if (err) throw err;
-
-    console.log("Portfolio complete! Check out index.html to see the output!");
-});*/
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -135,4 +128,14 @@ Add a New Project
     });
 };
 
-promptUser().then(promptProject).then(portfolioData => console.log(portfolioData));
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile("./index.html", pageHTML, err => {
+        if (err) throw new Error(err);
+
+        console.log("Portfolio complete! Check out index.html to see the output!");
+    });
+});
